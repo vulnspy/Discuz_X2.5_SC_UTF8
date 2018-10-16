c1e5b038e0a8cd9610e09f340fff2d5de235f59f
Ambulong <zeng.ambulong@gmail.com>
2018-10-16 23:48:02 +0800

    u

--- vsplatego.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-service mysqld start || service mysql start && mysql -u root -ptoor -e 'create database discuz;';
-mkdir /var/www/html/tmp;
-wget -O /var/www/html/tmp/Discuz_X2.5_SC_UTF8.zip http://test-1251457471.cosgz.myqcloud.com/Discuz_X2.5_SC_UTF8.zip && unzip -d /var/www/html/tmp /var/www/html/tmp/Discuz_X2.5_SC_UTF8.zip && mv /var/www/html/tmp/upload/* /var/www/html/ && rm -rf /var/www/html/tmp;
-chmod -R 777 /var/www/html;
